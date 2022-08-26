fab2167aa271a3ae929a92c557209e0b40c9834f
Kene_Okeke <kenekc18@gmail.com>
2022-08-26 20:52:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("pop_up");
+
+function openPopup(){
+    popup.classList.add("open_popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open_popup");
+}
